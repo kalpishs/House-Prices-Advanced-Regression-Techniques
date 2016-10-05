3e7929e5c4cf98708b4d5a8defae8e22cb9acd86
meghaag0408 <agar.megha18@gmail.com>
2016-10-05 20:36:12 +0530

    New Features Added

--- Initial Learning/RegularisedLinearRegression.py
+++ Initial Learning/RegularisedLinearRegression.py
@@ -11,23 +11,20 @@ from scipy.stats import skew
 import pylab as py
 from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
 
+from new_feature_creation import *
+
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 all_data = pd.concat((train.loc[:,'MSSubClass':'SaleCondition'], test.loc[:,'MSSubClass':'SaleCondition']))
 
 
 #Data Normalisation
-# matplotlib.rcParams['figure.figsize'] = (12.0, 6.0)
-# prices = pd.DataFrame({"price":train["SalePrice"], "log(price + 1)":np.log1p(train["SalePrice"])})
-# prices.hist()
-# plt.show()
 
 #log transform the target:
 train["SalePrice"] = np.log1p(train["SalePrice"])
 
 #log transform skewed numeric features:
 numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
-
 skewed_feats = train[numeric_feats].apply(lambda x: skew(x.dropna())) #compute skewness
 skewed_feats = skewed_feats[skewed_feats > 0.75]
 skewed_feats = skewed_feats.index
@@ -64,21 +61,9 @@ rmse_cv(model_lasso).mean()
 
 coef = pd.Series(model_lasso.coef_, index = X_train.columns)
 
-print("Lasso picked " + str(sum(coef != 0)) + " variables and eliminated the other " +  str(sum(coef == 0)) + " variables")
-imp_coef = pd.concat([coef.sort_values().head(10),
-                     coef.sort_values().tail(10)])
-
-matplotlib.rcParams['figure.figsize'] = (8.0, 10.0)
-imp_coef.plot(kind = "barh")
-plt.title("Coefficients in the Lasso Model")
-
-#let's look at the residuals as well:
-matplotlib.rcParams['figure.figsize'] = (6.0, 6.0)
 
 preds = pd.DataFrame({"preds":model_lasso.predict(X_train), "true":y})
 preds["residuals"] = preds["true"] - preds["preds"]
-#preds.plot(x = "preds", y = "residuals",kind = "scatter")
 preds = np.expm1(model_lasso.predict(X_test))
-
 solution = pd.DataFrame({"id":test.Id, "SalePrice":preds})
 solution.to_csv("ridge_sol.csv", index = False)
